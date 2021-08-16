f3acfbea2399e7ea2f9109b9365c3a5ae5f74220
Lawrence Wu <llwu@llwu.me>
2021-08-15 20:12:59 -0700

    Updates

---
+++ _next/static/u6lrQLt5o6IK6QA_WQYI8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/962-85de46b8c0137f5a9aaf.js","static/chunks/pages/index-d641a9309cdc077dea1f.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
